e103ead78bc7431850c7ca4a566795aabf4e3305
Vivek <viveksah59@gmail.com>
2024-02-22 19:45:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
+
+import { ErrorElement } from "./components";
+
+//loaders
+import { loader as landingLoader } from "./pages/Landing";
+
+//actions
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div
+              className="stat-title text-primary-content text-4xl font-bold
+          tracking tracking-widest"
+            >
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        At Comfy Store, we believe that furniture is more than just functional
+        pieces; it's an expression of your personality and lifestyle.
+        Established with a passion for craftsmanship and a commitment to
+        quality, we embark on a journey to redefine the way you experience your
+        living spaces.
+      </p>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        We understand that furniture is an investment in your home. That's why
+        we prioritize quality at every step of the manufacturing process. Our
+        furniture is not just about aesthetics – it's about durability,
+        functionality, and the joy it brings to your everyday life.
+      </p>
+    </>
+  );
+}
+
+export default About
+
+// Welcome to [Your Business Name] - Where Style Meets Comfort
+
+// At [Your Business Name], we believe that furniture is more than just functional pieces; it's an expression of your personality and lifestyle. Established with a passion for craftsmanship and a commitment to quality, we embark on a journey to redefine the way you experience your living spaces.
+
+// Our Story
+
+// Inspired by the idea that every home deserves the finest, our journey began with a simple yet profound vision – to bring elegance, comfort, and functionality to homes across [Your Location]. With a team of skilled artisans and designers, we set out to create furniture that not only stands the test of time but also adds a touch of sophistication to your living spaces.
+
+// Craftsmanship at its Best
+
+// Each piece of furniture at [Your Business Name] is a testament to our dedication to superior craftsmanship. From selecting the finest materials to employing traditional and modern techniques, we ensure that every item in our collection reflects our commitment to excellence.
+
+// Quality You Can Trust
+
+// We understand that furniture is an investment in your home. That's why we prioritize quality at every step of the manufacturing process. Our furniture is not just about aesthetics – it's about durability, functionality, and the joy it brings to your everyday life.
+
+// Our Promise
+
+// Customer satisfaction is at the core of our business. We strive to go beyond just selling furniture; we aim to create an experience that leaves a lasting impression. From personalized service to attention to detail, we are here to make your furniture buying journey seamless and enjoyable.
+
+// Explore Our Collection
+
+// Discover a curated selection of furniture that combines timeless design with modern functionality. Whether you're furnishing a new home or updating your current space, our diverse range has something to suit every taste and style.
+
+// Thank you for considering [Your Business Name] for your furniture needs. We look forward to being a part of your home's story and helping you create spaces that truly reflect who you are.
