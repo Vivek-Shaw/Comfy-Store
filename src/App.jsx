7ecedcfd7c431473e5675307b27b6aec65ae24f7
Vivek <viveksah59@gmail.com>
2024-06-11 14:34:33 +0530

    register done

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,9 @@ import { ErrorElement } from "./components";
 //loaders
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
-import { loader as productsLoader } from "./pages/Products"
+import { loader as productsLoader } from "./pages/Products";
 //actions
+import { action as registerAction } from "./pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -74,6 +75,7 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ]);
 
