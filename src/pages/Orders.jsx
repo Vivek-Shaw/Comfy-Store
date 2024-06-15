e7258975f90c04a391cac6d58e91a7bd37ee4bba
Vivek <viveksah59@gmail.com>
2024-06-15 16:31:30 +0530

    complex pagination added

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { redirect, useLoaderData } from "react-router-dom";
 import { toast } from "react-toastify";
 import { customFetch } from "../utils";
-import { OrdersList, PaginationContainer, SectionTitle } from "../components";
+import { OrdersList, ComplexPaginationContainer, SectionTitle } from "../components";
 
 export const loader =
   (store) =>
@@ -42,7 +42,7 @@ const Orders = () => {
     <>
       <SectionTitle text="Your Orders" />
       <OrdersList />
-      <PaginationContainer />
+      <ComplexPaginationContainer />
     </>
   );
 };
